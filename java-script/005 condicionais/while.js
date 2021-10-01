52a3bea2523b2f39bc13dcade8548ccbd51da0f2
cedric arnaud <79386450+CedricArnaud20@users.noreply.github.com>
2021-10-01 07:06:31 -0300

    Condicionais
    While e for

---
+++ java-script/005 condicionais/while.js
@@ -0,0 +1,19 @@
+//while
+let tab = 7;
+let cont = 1;
+
+while (cont <= 10) {
+    console.log(
+        tab, 'X' + cont, '=', 
+        tab*cont++ 
+    );
+}
+//for
+ let tab = 7;
+
+ for (let cont = 1; cont <= 10; cont++) {
+     console.log(
+         tab, 'X' + cont, '=', 
+         tab*cont
+     );
+ }
