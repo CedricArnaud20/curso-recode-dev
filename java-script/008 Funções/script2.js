bfe96ce642253b8253098bc701fe1bb02d1a3627
cedric arnaud <79386450+CedricArnaud20@users.noreply.github.com>
2021-10-03 23:06:44 -0300

    JS
    Funções

---
+++ java-script/008 Funções/script2.js
@@ -0,0 +1,14 @@
+function imprimirHora() {
+    let data = new Date();
+
+
+    let hora = data.getHours();
+    let min = data.getMinutes();
+    let seg = data.getSeconds();
+
+    let hora_formatada = hora + ':' + min + ':' + seg;
+
+    console.log(hora_formatada);
+}
+
+imprimirHora();
